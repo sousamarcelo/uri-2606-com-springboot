990bb95d4a5610d6a6619dc25dd385d6cb37fdda
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-02 00:58:33 -0300

    Testando consulta SQL com metodo customizado no repository + executando resultado da consulta no console com a interface ComandLine no programa principal

--- src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,11 +1,18 @@
 package com.devsuperior.uri2602.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 	
-	
+	@Query(nativeQuery = true, value = "SELECT name "
+			+ "FROM customers "
+			+ "WHERE state = :state") //[state] --> recebido pelo argumento do metodo
+	List<CustomerMinProjection> serch1(String state);
 
 }
